3ae618fbd4a27b086ca3b06ea2faa48c9ce31f14
Gerasimos Karathanos <gerasimos.karath@gmail.com>
2026-07-26 19:28:15 +0300

    Update silver_schema.sql

--- scripts/silver/silver_schema.sql
+++ scripts/silver/silver_schema.sql
@@ -1,16 +1,32 @@
 /*
-Create tables on silver layer but first truncate them if they already exists and then create the tables
+===============================================================================
+File        : silver_schema.sql
+Layer       : Silver
+
+Description
+-----------
+Creates the curated tables of the Silver layer.
+
+The Silver layer stores cleansed and standardized data after applying
+transformations to the raw Bronze datasets.
+
+Notes
+-----
+- Existing tables are dropped before recreation.
+- An audit column (dwh_create_date) is included in every table.
+- No business aggregations are stored in this layer.
+===============================================================================
 */
 
 IF OBJECT_ID('silver.crm_cust_info' , 'U') IS NOT NULL
 	DROP TABLE silver.crm_cust_info;
 CREATE TABLE silver.crm_cust_info (
-	cst_id INT,
-	cst_key NVARCHAR(50),
-	cst_firstname NVARCHAR(50),
-	cst_last_name NVARCHAR(50),
-	cst_marital NVARCHAR(50),
-	cst_gndr NVARCHAR(50),
+	cst_id 			INT,
+	cst_key 		NVARCHAR(50),
+	cst_firstname 	NVARCHAR(50),
+	cst_last_name 	NVARCHAR(50),
+	cst_marital 	NVARCHAR(50),
+	cst_gndr 		NVARCHAR(50),
 	cst_create_date DATE,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
@@ -18,14 +34,14 @@ CREATE TABLE silver.crm_cust_info (
 IF OBJECT_ID('silver.crm_prd_info' , 'U') IS NOT NULL
 	DROP TABLE silver.crm_prd_info;
 CREATE TABLE silver.crm_prd_info (
-	prd_id INT,
-	cat_id NVARCHAR(50),
-	prd_key NVARCHAR(50),
-	prd_nm NVARCHAR(50),
-	prd_cost INT,
-	prd_line NVARCHAR(50),
-	prd_start_dt DATE,
-	prd_end_dt DATE,
+	prd_id 			INT,
+	cat_id 			NVARCHAR(50),
+	prd_key 		NVARCHAR(50),
+	prd_nm 			NVARCHAR(50),
+	prd_cost 		INT,
+	prd_line 		NVARCHAR(50),
+	prd_start_dt 	DATE,
+	prd_end_dt 		DATE,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
@@ -33,15 +49,15 @@ CREATE TABLE silver.crm_prd_info (
 IF OBJECT_ID('silver.crm_sales_details' , 'U') IS NOT NULL
 	DROP TABLE silver.crm_sales_details;
 CREATE TABLE silver.crm_sales_details (
-	sls_ord_num NVARCHAR(50),
-	sls_prd_key NVARCHAR(50),
-	sls_cust_id INT,
-	sls_order_dt DATE,
-	sls_ship_dt DATE,
-	sls_due_dt DATE,
-	sls_sales INT,
-	sls_quantity INT,
-	sls_price INT,
+	sls_ord_num 	NVARCHAR(50),
+	sls_prd_key 	NVARCHAR(50),
+	sls_cust_id 	INT,
+	sls_order_dt 	DATE,
+	sls_ship_dt 	DATE,
+	sls_due_dt 		DATE,
+	sls_sales 		INT,
+	sls_quantity 	INT,
+	sls_price 		INT,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
@@ -59,8 +75,8 @@ CREATE TABLE silver.erp_cust_az12 (
 IF OBJECT_ID('silver.erp_loc_a101' , 'U') IS NOT NULL
 	DROP TABLE silver.erp_loc_a101;
 CREATE TABLE silver.erp_loc_a101 (
-	cid   NVARCHAR(50),
-	cntry NVARCHAR(50),
+	cid   			NVARCHAR(50),
+	cntry 			NVARCHAR(50),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
